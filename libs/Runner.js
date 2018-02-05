c3e0f8a1b4572b0dc2adb158881000b5de0b99c3
captainblue2013 <admin@lanhao.name>
2018-02-05 15:26:35 +0800

    fixed static path

--- libs/Runner.js
+++ libs/Runner.js
@@ -75,6 +75,10 @@ class Runner {
         await this.error(event, 'react build failed');
         return;
       }
+      if (shell.exec(`sed 's/\/static/static/g' ${`${process.cwd()}/build/index.html`}`).code !== 0) {
+        await this.error(event, 'react replace path');
+        return;
+      }
       process.chdir(`${process.cwd()}/build`);
       if (!fs.existsSync(`${process.cwd()}/Dockerfile`)) {
         //默认的静态项目 Dockerfile
@@ -87,7 +91,10 @@ class Runner {
         return;
       }
       if (shell.exec('npm run build').code !== 0) {
-        await this.error(event, 'react build failed');
+        await this.error(event, 'vue build failed');
+        return;
+      } if (shell.exec(`sed 's/\/static/static/g' ${`${process.cwd()}/dist/index.html`}`).code !== 0) {
+        await this.error(event, 'vue replace path');
         return;
       }
       process.chdir(`${process.cwd()}/dist`);
